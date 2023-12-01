074392f7d7c75e93227be125ce060b95a0df4b61
LightStrikeVex <151555861+LightStrikeVex@users.noreply.github.com>
2023-12-01 00:21:14 -0300

    Update new.lua

--- new.lua
+++ new.lua
@@ -1,25 +1,55 @@
--- Buscar el objeto "Shirt" en el WorkSpace
-local shirt = workspace:FindFirstChild("Shirt")
-
--- Buscar el objeto "Pants" en el WorkSpace
-local pants = workspace:FindFirstChild("Pants")
-
--- Verificar si se encontraron los objetos
-if shirt and pants then
-    -- Obtener información de ShirtTemplate
-    local shirtTemplate = shirt:FindFirstChild("ShirtTemplate")
-    
-    -- Obtener información de PantsTemplate
-    local pantsTemplate = pants:FindFirstChild("PantsTemplate")
-
-    -- Verificar si se encontraron las plantillas
-    if shirtTemplate and pantsTemplate then
-        -- Imprimir la información en la consola
-        print("Información de ShirtTemplate:", shirtTemplate.Value)
-        print("Información de PantsTemplate:", pantsTemplate.Value)
-    else
-        print("No se encontraron las plantillas.")
+-- Función para buscar un objeto por nombre en un modelo
+local function findObjectInModel(model, objectName)
+    local object = model:FindFirstChild(objectName)
+    if object then
+        return object
     end
-else
-    print("No se encontraron los objetos Shirt y/o Pants en el WorkSpace.")
+
+    for _, child in ipairs(model:GetChildren()) do
+        if child:IsA("Model") then
+            object = findObjectInModel(child, objectName)
+            if object then
+                return object
+            end
+        end
+    end
+
+    return nil
 end
+
+-- Función principal para buscar y obtener información
+local function searchAndPrintInfo()
+    -- Recorrer todos los modelos en el Workspace
+    for _, model in ipairs(workspace:GetChildren()) do
+        if model:IsA("Model") then
+            -- Buscar el objeto "Shirt" en el modelo actual
+            local shirt = findObjectInModel(model, "Shirt")
+            
+            -- Buscar el objeto "Pants" en el modelo actual
+            local pants = findObjectInModel(model, "Pants")
+
+            -- Verificar si se encontraron los objetos
+            if shirt and pants then
+                -- Obtener información de ShirtTemplate
+                local shirtTemplate = shirt:FindFirstChild("ShirtTemplate")
+                
+                -- Obtener información de PantsTemplate
+                local pantsTemplate = pants:FindFirstChild("PantsTemplate")
+
+                -- Verificar si se encontraron las plantillas
+                if shirtTemplate and pantsTemplate then
+                    -- Imprimir la información en la consola
+                    print("Información de ShirtTemplate:", shirtTemplate.Value)
+                    print("Información de PantsTemplate:", pantsTemplate.Value)
+                else
+                    print("No se encontraron las plantillas en el modelo:", model.Name)
+                end
+            else
+                print("No se encontraron los objetos Shirt y/o Pants en el modelo:", model.Name)
+            end
+        end
+    end
+end
+
+-- Llamar a la función principal
+searchAndPrintInfo()
